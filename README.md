575e20bef02e93bcf0d28ce05a5da2a59aee0dbd
Mehdi El Gueddari <mehdi@shhmooze.com>
2014-01-23 01:36:46 +0000

    Part 03

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ This is the accompanying code for the [Generating HTML emails with RazorEngine](
 
 Quick description of each project:
 
-1. Introduction
-2. Basics: generating your first email
-3. More Basics: using the cache, VS integration & more (TBD) 
+1. [Introduction](http://mehdi.me/generating-html-emails-with-razorengine-introduction/)
+2. [Basics: generating your first email](http://mehdi.me/generating-html-emails-with-razorengine-basics-generating-your-first-email/)
+3. [More Basics: caching, VS integration & namespace config ](http://mehdi.me/generating-html-emails-with-razorengine-part-03-caching-vs-integration-namespace-config/)
 4. Taking a step back: what is Razor and what does RazorEngine actually do? (TBD)
 5. URL generation & T4MVC integration with RazorEngine (TBD)
 6. Layout and partials with RazorEngine (TBD)
-7. On keeping your sanity: inlining CSS with PreMailer.Net (TBD)
-8. Getting a little help from CsQuery: including subject, sender and recipient information in your email templates (TBD)
-9. Putting it all together: building a complete email-generation library with RazorEngine, PreMailer.Net and CsQuery (TBD)
+7. Layout and partials with RazorEngine (TBD)
+8. On keeping your sanity: inlining CSS with PreMailer.Net (TBD)
+9. Getting a little help from CsQuery: including subject, sender and recipient information in your email templates (TBD)
+10. Putting it all together: building a complete email-generation library with RazorEngine, PreMailer.Net and CsQuery (TBD)
 
 Refer to the blog for the details.
---
+++ src/Part 03/ConsoleApplication/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication.Models;
+using RazorEngine;
+using RazorEngine.Configuration.Xml;
+using RazorEngine.Templating;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static readonly string TemplateFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EmailTemplates");
+
+        static void Main(string[] args)
+        {
+            /*
+             * Quick benchmark demonstrating the effect of using RazorEngine's caching functionality.
+             * 
+             * We'll generate the same email 3 times in a row - first without caching and then with caching.
+             * We'll measure and print how long each try takes.
+             */
+
+            var welcomeEmailTemplate = File.ReadAllText(Path.Combine(TemplateFolderPath, "WelcomeEmail.cshtml"));
+            var model = new UserModel() { Name = "Sarah", Email = "sarah@mail.example", IsPremiumUser = false };
+
+            var templateService = new TemplateService();
+
+            // Add the default namespaces that will be automatically imported in all template classes
+            templateService.AddNamespace("ConsoleApplication.Models");
+
+            //-- Warm-up
+            var emailHtmlBody = templateService.Parse(welcomeEmailTemplate, model, null, null);
+            Stopwatch watch = new Stopwatch();
+
+            //-- Without cache
+            Console.WriteLine("Without cache: ");
+            for (int i = 1; i < 4; i++)
+            {
+                watch.Start();
+                emailHtmlBody = templateService.Parse(welcomeEmailTemplate, model, null, null);
+                watch.Stop();
+
+                Console.WriteLine("Try #{0}: {1}ms", i, watch.ElapsedMilliseconds);
+                watch.Reset();
+            }
+
+            //-- With cache
+            Console.WriteLine("With cache: ");
+            for (int i = 1; i < 4; i++)
+            {
+                watch.Start();
+                emailHtmlBody = templateService.Parse(welcomeEmailTemplate, model, null, "Welcome");
+                watch.Stop();
+
+                Console.WriteLine("Try #{0}: {1}ms", i, watch.ElapsedMilliseconds);
+                watch.Reset();
+            }
+
+            Console.ReadLine();
+
+            /* 
+             * Example output:
+             * 
+Without cache:
+Try #1: 116ms
+Try #2: 114ms
+Try #3: 118ms
+With cache:
+Try #1: 114ms
+Try #2: 0ms
+Try #3: 0ms
+             * 
+             */
+        }
+    }
+}
